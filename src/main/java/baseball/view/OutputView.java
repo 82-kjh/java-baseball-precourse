ba036d5b993426d869a6acebc133699f11d9ed24
jaehyeok <jaehyeok@jaehyeok-macbook-air.local>
2025-12-13 19:07:06 +0900

    refactor(outputView): mvc 패턴으로의 리팩토링에 따른 클래스 생성
    - 유저에게 보여줄 출력 클래스를 생성함.

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,32 @@
+package baseball.view;
+
+import java.io.Console;
+
+public class OutputView {
+    /**
+     * 출력해야 하는 메시지
+     * "숫자를 입력해주세요 : "
+     * "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+     * "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+     */
+
+    /**
+     * static: 객체를 생성할 때마다 문자열을 메모리에 새로 생성하지 않음. 메모리 절약
+     * final: 고정 문구이므로 값이 바뀌지 않음
+     */
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String ASK_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void printInputMessage() {
+        System.out.println(INPUT_MESSAGE);
+    }
+
+    public void printEndMessage() {
+        System.out.println(END_MESSAGE);
+    }
+
+    public void printAskRestartMessage() {
+        System.out.println(ASK_RESTART_MESSAGE);
+    }
+}
