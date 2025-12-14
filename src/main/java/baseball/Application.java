acd06e03bf5455f3aa805bba0d97db564bf74746
82-kjh <kjhyeok0123@gmail.com>
2025-12-14 17:04:55 +0900

    Revert "82 kjh"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,22 +1,7 @@
 package baseball;
 
-import baseball.controller.GameController;
-import baseball.domain.NumberGenerator;
-import baseball.domain.RandomNumberGenerator;
-import baseball.domain.Referee;
-import baseball.view.InputView;
-import baseball.view.OutputView;
-
 public class Application {
     public static void main(String[] args) {
-
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        NumberGenerator numberGenerator = new RandomNumberGenerator();
-        Referee referee = new Referee();
-
-        GameController gameController = new GameController(inputView, outputView, numberGenerator, referee);
-        gameController.run();
-
+        //TODO: 숫자 야구 게임 구현
     }
 }
